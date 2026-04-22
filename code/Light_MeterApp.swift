8bba73f6d8e417e69fed0d0f188ff8d8906e2d84
danielplutchak <158518023+danielplutchak@users.noreply.github.com>
2026-04-22 14:56:06 -0500

    renamed main.swift to Light_MeterApp.swift

---
+++ code/Light_MeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_MeterApp.swift
+//  Light Meter
+//
+//  Created by Daniel Plutchak on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Light_MeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
