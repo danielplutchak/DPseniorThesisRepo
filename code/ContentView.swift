3b6135bd5dcce4a52953cee18434b6ca95ee543e
danielplutchak <158518023+danielplutchak@users.noreply.github.com>
2026-04-22 14:54:27 -0500

    Updated file

--- code/ContentView.swift
+++ code/ContentView.swift
@@ -1,11 +1,37 @@
+//
+//  ContentView.swift
+//  Light Meter
+//
+//  Created by Daniel Plutchak on 10/29/25.
+//
+
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
+    @StateObject private var model = FrameHandler()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            FrameView(image: model.currentFrame)
+                .padding()
+            
+            //wheel view and functionality
+            EVSettingsUIView(model: model)
+
+        }
+        .padding()
+        .background(Gradient(colors: gradientColors))
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
